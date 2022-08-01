e3ecb2a6768790a37bdbab3d1df050ee7a5d8bd1
zuhair-credencesoft <zuhair@credencesoft.in>
2022-08-01 11:26:24 +0530

    Login Page Created.

--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,83 @@
-
-import React from "react"
+import React from "react";
 import styles from "../styles/Login.module.css";
 import Link from "next/link";
+import { Form } from "react-bootstrap";
 
-import { useState } from 'react';
+import { useState } from "react";
 import { Button, CardContent, Card, Grid } from "@mui/material";
-function Login() {
-    return (
-        <div>
-           <div className={styles.bgimage}>
-                <div className={styles.card}>
-                    <div>
-                        <img className={styles.image} src="https://app.bookonelocal.in/assets/images/sortlogo/Logo_Bookone.png" alt="" />
-                    </div>
-                    <div></div>
+const Login = () => {
+  return (
+    <div className={styles.bgimage}>
+      <div className={styles.bigContainer}>
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <div className={styles.leftContainer}>
+              <img
+                className={styles.logoImage}
+                src="https://app.bookonelocal.in/assets/images/sortlogo/Logo_Bookone.png"
+                alt=""
+              />
+              <div className={styles.extraButtons}>
+                <img
+                  className={styles.extraButtonImg}
+                  src="https://app.bookonelocal.in/assets/images/playdownload.png"
+                  alt=""
+                />
+                <img
+                  className={styles.extraButtonImg}
+                  src="https://app.bookonelocal.in/assets/images/appdownload.png"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className={styles.rightContainer}>
+              <form className={styles.form}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <div className={styles.buttonContainer}>
+                  <button className={styles.submitbutton}>Sign in</button>
+                  <button className={styles.submitbutton}>
+                    Register Your Business
+                  </button>
+                  <h3 className={styles.forgot}>Forgot password</h3>
                 </div>
-           </div>
+              </form>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
 
-
+{
+  /* <div className={styles.bgimage}>
+  <div className={styles.cardContainer}>
+    <div className={styles.imageContainer}>
+      {/* <img
+              className={styles.image}
+              src="https://app.bookonelocal.in/assets/images/sortlogo/Logo_Bookone.png"
+              alt=""
+            /> */
+}
+// </div>
+//     <div className={styles.card}>
+//       <div className={styles.leftContainer}>
+//         <h2>Logo</h2>
+//         <div>
+//           <h3>Google</h3>
+//           <h4>Apple</h4>
+//         </div>
+//       </div>
+//       <div className={styles.rightContainer}>All Data</div>
+//     </div>
+//   </div>
+// </div>; */}
--- pages/property/[property].js
+++ pages/property/[property].js
@@ -1,63 +1,76 @@
-import Table from 'react-bootstrap/Table';
-import React from "react"
+import Table from "react-bootstrap/Table";
+import React from "react";
 import styles from "../../styles/Property.module.css";
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 import { BsSearch } from "react-icons/bs";
 import { AiFillCaretDown } from "react-icons/ai";
 import Link from "next/link";
 
-import { useState } from 'react';
-function Property({ oldProperties,property }) {
-    // console.log(oldProperties.roomDtos)
-    // console.log(property)
-    const [shopModal, setshopModal] = useState(false);
+import { useState } from "react";
+function Property({ oldProperties, property }) {
+  console.log(oldProperties.roomDtos);
+  // console.log(property)
+  const [shopModal, setshopModal] = useState(false);
 
-    const handleShopModal = () => {
-        setshopModal(!shopModal)
-    }
+  const handleShopModal = () => {
+    setshopModal(!shopModal);
+  };
 
-    return (
-        <Container>
-            <Table bordered className={styles.container}>
-                <thead className={styles.heading}>
-                    <tr className={styles.heading}>
-                        <th>Channel Manager Room Type</th>
-                        <th>Channel Manager Room Rate</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className={styles.heading}>
-                        <td>
-                            <button onClick={handleShopModal} className={styles.button}>All Room Types <AiFillCaretDown />
-                                <div onMouseLeave={handleShopModal} className={styles.shopModal} style={shopModal ? { display: 'block' } : { display: 'none' }}>
-                                    <Link href={`${property}`}><li>All</li></Link>
-                                    {oldProperties.roomDtos.map((val, i) => {
-                                        return (
-                                            <Link key={i} href={`${property}`}><li>{val.name}</li></Link>
-                                        )
-                                    })}
-                                </div>
-                            </button></td>
-                        <td>
-                            <div className={styles.inputContainer}>
-                                <i className={styles.icon}><BsSearch className={styles.searchIcon} /></i>
-                                <input className={styles.inputField} type="text" placeholder="Username" name="usrnm" />
-                            </div>
-                        </td>
-                    </tr>
-                    {oldProperties.roomDtos.map((val, i) => {
-                        return (
-                            <tr key={i}>
-                                <td>
-                                    {val.name}
-                                </td>
-                                <td>
-                                    Plan Rooms
-                                </td>
-                            </tr>
-                        )
-                    })}
-                    {/* <tr>
+  return (
+    <Container>
+      <Table bordered className={styles.container}>
+        <thead className={styles.heading}>
+          <tr className={styles.heading}>
+            <th>Channel Manager Room Type</th>
+            <th>Channel Manager Room Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={styles.heading}>
+            <td>
+              <button onClick={handleShopModal} className={styles.button}>
+                All Room Types <AiFillCaretDown />
+                <div
+                  onMouseLeave={handleShopModal}
+                  className={styles.shopModal}
+                  style={shopModal ? { display: "block" } : { display: "none" }}
+                >
+                  <Link href={`${property}`}>
+                    <li>All</li>
+                  </Link>
+                  {oldProperties.roomDtos.map((val, i) => {
+                    return (
+                      <Link key={i} href={`${property}`}>
+                        <li>{val.name}</li>
+                      </Link>
+                    );
+                  })}
+                </div>
+              </button>
+            </td>
+            <td>
+              <div className={styles.inputContainer}>
+                <i className={styles.icon}>
+                  <BsSearch className={styles.searchIcon} />
+                </i>
+                <input
+                  className={styles.inputField}
+                  type="text"
+                  placeholder="Username"
+                  name="usrnm"
+                />
+              </div>
+            </td>
+          </tr>
+          {oldProperties.roomDtos.map((val, i) => {
+            return (
+              <tr key={i}>
+                <td>{val.name}</td>
+                <td>Plan Rooms</td>
+              </tr>
+            );
+          })}
+          {/* <tr>
                         <td rowSpan={3}>
                             Double Room
                         </td>
@@ -93,43 +106,41 @@ function Property({ oldProperties,property }) {
                             Honeymoon
                         </td>
                     </tr> */}
-                </tbody>
-            </Table>
-        </Container>
-    );
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
 
 export default Property;
 
-
 export async function getStaticPaths() {
-    return {
-        paths: [
-            { params: { property: "237" } },
-            { params: { property: "368" } },
-            { params: { property: "495" } },
-        ],
-        fallback: false,
-    };
+  return {
+    paths: [
+      { params: { property: "237" } },
+      { params: { property: "368" } },
+      { params: { property: "495" } },
+    ],
+    fallback: false,
+  };
 }
 
 export async function getStaticProps(context) {
-    const { property } = context.params;
-    const propertiesResponse = await fetch(`https://api.bookonelocal.in/channel-integration/api/channelManager/property/${property}`,
-        {
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                Authorization:
-                    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJib29rb25ldGVzdGJ1c2luZXNzQGdtYWlsLmNvbSIsInNjb3BlcyI6IlJPTEVfUFJPUF9BRE1JTiIsImlhdCI6MTY1ODg5Njk5OCwiZXhwIjoxNjU5MzI4OTk4fQ.yJpc1N9tn_q345k3hZHLapQaeXVO23xlWkbQwhPx7XI",
-                "Content-Type": "application/x-www-form-urlencoded",
-                APP_ID: "BOOKONE_WEB_APP",
-            },
-        });
-    const oldProperties = await propertiesResponse.json();
+  const { property } = context.params;
+  const propertiesResponse = await fetch(
+    `https://api.bookonelocal.in/channel-integration/api/channelManager/property/${property}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJib29rb25ldGVzdGJ1c2luZXNzQGdtYWlsLmNvbSIsInNjb3BlcyI6IlJPTEVfUFJPUF9BRE1JTiIsImlhdCI6MTY1ODg5Njk5OCwiZXhwIjoxNjU5MzI4OTk4fQ.yJpc1N9tn_q345k3hZHLapQaeXVO23xlWkbQwhPx7XI",
+        "Content-Type": "application/x-www-form-urlencoded",
+        APP_ID: "BOOKONE_WEB_APP",
+      },
+    }
+  );
+  const oldProperties = await propertiesResponse.json();
 
-
-    return { props: { oldProperties, property } };
-
+  return { props: { oldProperties, property } };
 }
-
