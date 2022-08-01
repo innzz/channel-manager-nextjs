b34491d73f756f8b77895ecadeab219b5ad20bea
rekha-credencesoft <rekha@credencesoft.in>
2022-08-01 11:57:01 +0530

    Added validation in login page

--- pages/login.js
+++ pages/login.js
@@ -1,72 +1,92 @@
 import React from "react";
 import styles from "../styles/Login.module.css";
 import Link from "next/link";
-import { Form } from "react-bootstrap";
-
+import { Alert, Form } from "react-bootstrap";
+import { useRouter } from 'next/router'
 import { useState } from "react";
 import { Button, CardContent, Card, Grid } from "@mui/material";
 const Login = () => {
-  return (
-    <div className={styles.bgimage}>
-      <div className={styles.bigContainer}>
-        <div className={styles.container}>
-          <div className={styles.card}>
-            <div className={styles.leftContainer}>
-              <img
-                className={styles.logoImage}
-                src="https://app.bookonelocal.in/assets/images/sortlogo/Logo_Bookone.png"
-                alt=""
-              />
-              <div className={styles.extraButtons}>
-                <img
-                  className={styles.extraButtonImg}
-                  src="https://app.bookonelocal.in/assets/images/playdownload.png"
-                  alt=""
-                />
-                <img
-                  className={styles.extraButtonImg}
-                  src="https://app.bookonelocal.in/assets/images/appdownload.png"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className={styles.rightContainer}>
-              <form className={styles.form}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <div className={styles.buttonContainer}>
-                  <button className={styles.submitbutton}>Sign in</button>
-                  <button className={styles.submitbutton}>
-                    Register Your Business
-                  </button>
-                  <h3 className={styles.forgot}>Forgot password</h3>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [alert, setAlert] = useState(false);
+    const router = useRouter()
+    const signIn = (e) => {
+        e.preventDefault()
+        if (email === "bookone@gmail.com" && password == "bookone") {
+            return (
+                router.push("property/237")
+            )
+        }
+        else {
+            setAlert(true)
+        }
+    }
+    return (
+        <div className={styles.bgimage}>
+            <div className={styles.bigContainer}>
+                <div className={styles.container}>
+                    <div className={styles.card}>
+                        <div className={styles.leftContainer}>
+                            <img
+                                className={styles.logoImage}
+                                src="https://app.bookonelocal.in/assets/images/sortlogo/Logo_Bookone.png"
+                                alt=""
+                            />
+                            <div className={styles.extraButtons}>
+                                <img
+                                    className={styles.extraButtonImg}
+                                    src="https://app.bookonelocal.in/assets/images/playdownload.png"
+                                    alt=""
+                                />
+                                <img
+                                    className={styles.extraButtonImg}
+                                    src="https://app.bookonelocal.in/assets/images/appdownload.png"
+                                    alt=""
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.rightContainer}>
+                            <form className={styles.form}>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control value={email} type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control value={password} type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                                </Form.Group>
+                                <div className={styles.buttonContainer}>
+                                    <button className={styles.submitbutton} onClick={signIn}>Sign in</button>
+                                    <button className={styles.submitbutton}>
+                                        Register Your Business
+                                    </button>
+                                    <Alert show={alert} variant="danger" onClose={() => setAlert(false)} dismissible>
+                                        <p>
+                                            Email Id or Password is incorrect
+                                        </p>
+                                    </Alert>
+                                    <h3 className={styles.forgot}>Forgot password</h3>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-              </form>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
 
 {
-  /* <div className={styles.bgimage}>
-  <div className={styles.cardContainer}>
-    <div className={styles.imageContainer}>
-      {/* <img
-              className={styles.image}
-              src="https://app.bookonelocal.in/assets/images/sortlogo/Logo_Bookone.png"
-              alt=""
-            /> */
+    /* <div className={styles.bgimage}>
+    <div className={styles.cardContainer}>
+      <div className={styles.imageContainer}>
+        {/* <img
+                className={styles.image}
+                src="https://app.bookonelocal.in/assets/images/sortlogo/Logo_Bookone.png"
+                alt=""
+              /> */
 }
 // </div>
 //     <div className={styles.card}>
