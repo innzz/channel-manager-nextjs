b6407dc7af988a276b10fe02297312ddb8982ea5
Inzamam <inzamam@credencesoft.in>
2022-08-12 19:27:52 +0700

    Added bookings page

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,7 +20,7 @@ function NavBar() {
     return (
         <div className={styles.navbarContainer}>
             <Col className={styles.logo}>
-                <Image className={styles.logoImage} height={60} width={150} src='/logo.svg' />
+                <Link href={'/'}><Image className={styles.logoImage} height={60} width={150} src='/logo.svg' /></Link>
             </Col>
             <Col className={styles.navItem}>
                 <div className={styles.links}>
@@ -45,6 +45,7 @@ function NavBar() {
                             <li><CgProfile height={50} width={50} /> <span>My Account</span></li>
                             <li><TbNotes height={50} width={50} /><span>Terms & Condition </span></li>
                             <li><MdSupportAgent height={50} width={50} /><span>Support</span></li>
+                            <Link href={'/bookings'}><li><MdSupportAgent height={50} width={50} /><span>Bookings</span></li></Link>
                             <li><AiOutlineLogout height={50} width={50} /><span>Logout</span></li>
                         </div>
 
---
+++ pages/bookings.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import styles from '../styles/Bookings.module.css';
+import {BiArrowBack,BiSearch} from 'react-icons/bi';
+import {MdAddCircle} from 'react-icons/md';
+import {AiFillCaretDown} from 'react-icons/ai';
+import {BsThreeDotsVertical} from 'react-icons/bs';
+import { Col, Row } from 'react-bootstrap';
+
+function bookings() {
+  return (
+    <div className={styles.bookingsOuterContainer}>
+    <Navbar />
+    <div className={styles.bookingsMainContainer}>
+        <div className={styles.bookingsSearchSection}>
+            <span className={styles.bookingsSearchSectionBookingsMngmtSpan}><BiArrowBack className={styles.bookingsSearchSectionBookingsMngmtIcon} />Booking Management</span>
+            <span className={styles.bookingsSearchSectionBookingsInputSpan}><BiSearch className={styles.bookingsSearchSectionIcon} size={26}/><input className={styles.bookingsSearchSectionInput} placeholder='Search bookings' /></span>
+        </div>
+        <div className={styles.bookingsTableSection}>
+            <div className={styles.bookingsTableButtonsSection}>
+                <button><MdAddCircle size={20} className={styles.bookingsTableButtonIcon}/> A Room</button>
+                <button><MdAddCircle size={20} className={styles.bookingsTableButtonIcon}/> Property</button>
+                <button><MdAddCircle size={20} className={styles.bookingsTableButtonIcon}/> Enquiry</button>
+                <span className={styles.bookingsTableButtonsGroup}>
+                    <button>Recent Booking</button>
+                    <button>Todays Arrival</button>
+                    <button>In House</button>
+                    <button>Todays Departure</button>
+                </span>
+            </div>
+            <div className={styles.bookingsTableDropdownsSection}>
+                <span className={styles.bookingsTableDropdown}><span>All</span><AiFillCaretDown /></span>
+                <span className={styles.bookingsTableDropdown}><span>All</span><AiFillCaretDown /></span>
+                <span className={styles.bookingsTableDropdown}><span>All</span><AiFillCaretDown /></span>
+                <span className={styles.bookingsTableDropdown}><span>All</span><AiFillCaretDown /></span>
+                <span className={styles.bookingsTableDropdown}><span>All</span><AiFillCaretDown /></span>
+                <span className={styles.bookingsTableDropdown}><span>All</span><AiFillCaretDown /></span>
+                <button>Reset</button>
+            </div>
+            <div className={styles.bookingsTable}>
+                <Row className={styles.bookingsTableHeadingRow}>
+                    <Col><span>Action</span></Col>
+                    <Col><span>Reservation Id</span></Col>
+                    <Col><span>Name</span></Col>
+                    <Col><span>Booking Date</span></Col>
+                    <Col><span>Arrival</span></Col>
+                    <Col><span>Departure</span></Col>
+                    <Col><span>Room Type/Room No</span></Col>
+                    <Col><span>Source</span></Col>
+                    <Col><span>Status</span></Col>
+                    <Col><span>Payable Amount</span></Col>
+                    <Col><span>Note</span></Col>
+                    <Col><span>Action</span></Col>
+                </Row>
+                <Row className={styles.bookingsTableInnerRow}>
+                    <Col><span><BsThreeDotsVertical /></span></Col>
+                    <Col><span>BST-B-1690</span></Col>
+                    <Col><span>Inzamam Pawaskar</span></Col>
+                    <Col><span>Aug 12, 2022 to Aug 13, 2022</span></Col>
+                    <Col><span>Fri, Aug 12,01:30 PM<span>ETA</span></span></Col>
+                    <Col><span>Fri, Aug 12,01:30 PM<span>ETA</span></span></Col>
+                    <Col><span>Double Room 302</span></Col>
+                    <Col><span>Walkin</span></Col>
+                    <Col><span>CONFIRMED</span></Col>
+                    <Col><span>₹4,000.00</span></Col>
+                    <Col><span></span></Col>
+                    <Col><span><BsThreeDotsVertical /></span></Col>
+                </Row>
+                <Row className={styles.bookingsTableInnerRow}>
+                    <Col><span><BsThreeDotsVertical /></span></Col>
+                    <Col><span>BST-B-1690</span></Col>
+                    <Col><span>Inzamam Pawaskar</span></Col>
+                    <Col><span>Aug 12, 2022 to Aug 13, 2022</span></Col>
+                    <Col><span>Fri, Aug 12,01:30 PM<span>ETA</span></span></Col>
+                    <Col><span>Fri, Aug 12,01:30 PM<span>ETA</span></span></Col>
+                    <Col><span>Double Room 302</span></Col>
+                    <Col><span>Walkin</span></Col>
+                    <Col><span>CONFIRMED</span></Col>
+                    <Col><span>₹4,000.00</span></Col>
+                    <Col><span></span></Col>
+                    <Col><span><BsThreeDotsVertical /></span></Col>
+                </Row>
+                <Row className={styles.bookingsTableInnerRow}>
+                    <Col><span><BsThreeDotsVertical /></span></Col>
+                    <Col><span>BST-B-1690</span></Col>
+                    <Col><span>Inzamam Pawaskar</span></Col>
+                    <Col><span>Aug 12, 2022 to Aug 13, 2022</span></Col>
+                    <Col><span>Fri, Aug 12,01:30 PM<span>ETA</span></span></Col>
+                    <Col><span>Fri, Aug 12,01:30 PM<span>ETA</span></span></Col>
+                    <Col><span>Double Room 302</span></Col>
+                    <Col><span>Walkin</span></Col>
+                    <Col><span>CONFIRMED</span></Col>
+                    <Col><span>₹4,000.00</span></Col>
+                    <Col><span></span></Col>
+                    <Col><span><BsThreeDotsVertical /></span></Col>
+                </Row>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default bookings
