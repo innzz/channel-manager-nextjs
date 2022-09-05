ac3edd21a011c9645ec3b04d8d53f55f898c1aa7
zuhair-credencesoft <zuhair@credencesoft.in>
2022-09-05 15:28:44 +0530

    Error Fixed.

---
+++ pages/mmtPostRequest.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MMT = () => {
+  return (
+    <div>
+      <h1>MMT POST Request</h1>
+      <div className="firstContainer">
+        <input type="text" name="name" />
+      </div>
+    </div>
+  );
+};
+
+export default MMT;
