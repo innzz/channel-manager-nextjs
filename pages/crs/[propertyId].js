b69b62f62a20cbf539c1619d70b1d0f69f6c87cf
rekha-credencesoft <rekha@credencesoft.in>
2022-09-12 10:24:48 +0530

    Added dynamic paths and changed the redirecting positions

--- pages/crs/[propertyId].js
+++ pages/crs/[propertyId].js
@@ -290,7 +290,7 @@ export default function PropertyId() {
 
   return (
     <div className={styles.outerContainer}>
-      <Navbar />
+      <Navbar propertyId={propertyId} />
       <div className={styles.container}>
         <Head>
           <title>MMT Channel Manager</title>
